69a82d4db14716544737fa3b08845e6119f5f003
Saurabh Maurya <saurabh.maurya.ece20@itbhu.ac.in>
2023-10-02 15:03:09 +0530

    added history

---
+++ src/components/History.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./History.css";
+
+const History = ({setShowHistory}) => {
+  
+  const history = JSON.parse(localStorage.getItem("history")) || [];
+
+  return (
+    <div className="history-container">
+      <div className="history-header">
+        <h2 className="history-title">Calculation History</h2>
+        <button className="close-button" onClick={() => setShowHistory(false)}>
+          X
+        </button>
+      </div>
+      <ul className="history-list">
+        {history.slice().reverse().map((item, index) => (
+          <li key={index} className="history-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default History;
