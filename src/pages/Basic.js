16f405d20abb7073cd319859a8781c9f97f1aaea
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-10-03 16:27:08 +0530

    quick changes

--- src/pages/Basic.js
+++ src/pages/Basic.js
@@ -1,57 +1,50 @@
 import React, {useState} from "react"
 import Button from "../components/Button";
+import History from "../components/History";
 
 const Basic = () => {
-    const [display, setDisplay] = useState("0");
-    const [equation, setEquation] = useState()
-  return (
-    <div className="calculator">
-        <div className="display">{display}</div>
-            <div className="buttons">
-            <div className="buttonContainer">
-            <Button text="AC" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="sin" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="cos" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="tan" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
+const [display, setDisplay] = useState("0");
+    const [equation, setEquation] = useState();
+    const [showHistory, setShowHistory] = useState(false);
+
+    const buttonRows = [
+        ["AC", "sin", "cos", "tan"],
+        ["arcsin", "arccos", "arctan", "x"],
+        ["+/-", "%", "÷", "-"],
+        ["7", "8", "9", "+"],
+        ["4", "5", "6", "^"],
+        ["1", "2", "3", "^"],
+        ["0", ".", "="],
+    ];
+
+    const renderButton = (text) => (
+        <Button
+            key={text}
+            text={text}
+            display={display}
+            setDisplay={setDisplay}
+            equation={equation}
+            setEquation={setEquation}
+        />
+    );
+
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="display">
+                    <img id="history" src="https://cdn-icons-png.flaticon.com/512/61/61122.png" width="20px" height="20px" onClick={()=>setShowHistory(true)}/>
+                    {display}
+                </div>
+                <div className="buttons">
+                    {buttonRows.map((row) => (
+                        <div className="buttonContainer" key={row.join("")}>
+                            {row.map(renderButton)}
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className="buttonContainer">
-            <Button text="arcsin" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="arccos" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="arctan" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="x" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            </div>
-            <div className="buttonContainer">
-            <Button text="+/-" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="%" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="÷" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="-" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            </div>
-            <div className="buttonContainer">
-            <Button text="7" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="8" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="9" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="+" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            </div>
-            <div className="buttonContainer">
-            <Button text="4" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="5" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="6" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="^" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            </div>
-            <div className="buttonContainer">
-            <Button text="1" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="2" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="3" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="^" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            </div>
-            <div className="buttonContainer">
-            <Button text="0" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="." display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            <Button text="=" display={display} setDisplay={setDisplay} equation={equation} setEquation={setEquation} />
-            
-            </div>
-            </div>
-    </div>
+            {showHistory && <History setShowHistory={setShowHistory} />}
+        </div>
   )
 };
 
