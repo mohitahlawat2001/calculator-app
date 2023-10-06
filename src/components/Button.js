e2732bacf99bb259396ec9dbd6cd28c796e90579
Rajat Rajput <rajat.contact.rajat@gmail.com>
2023-10-06 09:19:33 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,7 @@ import "../App.css";
 
 const Button = ({ text, setDisplay, display, equation, setEquation }) => {
   const handleClick = () => {
-  let history = JSON.parse(localStorage.getItem('history')) || [];
-
-
+    let history = JSON.parse(localStorage.getItem("history")) || [];
 
     const calculateTrigonometric = (operation) => {
       let result;
@@ -31,25 +29,33 @@ const Button = ({ text, setDisplay, display, equation, setEquation }) => {
           result = "";
       }
       history.push(operation + "(" + display + ") = " + result);
-      if(history.length > 10){
-          history.shift();
+      if (history.length > 10) {
+        history.shift();
       }
-      localStorage.setItem('history', JSON.stringify(history));
+      localStorage.setItem("history", JSON.stringify(history));
       setDisplay(result.toString());
       setEquation(result.toString());
     };
 
-
-    if (display === "0" && "+-x÷%+/-.".includes(text)) { // if the display is 0 and the button is an operator, don't add the operator
-      return
+    if (display === "0" && "+-x÷%+/-.".includes(text)) {
+      // if the display is 0 and the button is an operator, don't add the operator
+      return;
     } else if (display === "0" && text !== "AC") {
       setDisplay(text);
       setEquation(text);
-    } else if ("+-x÷%+/-.".includes(text) && "+-x÷%+/-.".includes(display[display.length - 1])) { // if the last character is an operator, don't add another operator
-      return
-    } else if (typeof (parseInt(display)) === "number" && typeof (text) === "number") { // if the last character is a number, don't add another number
-      setDisplay(text)
-      setEquation(text)
+    } else if (
+      "+-x÷%+/-.".includes(text) &&
+      "+-x÷%+/-.".includes(display[display.length - 1])
+    ) {
+      // if the last character is an operator, don't add another operator
+      return;
+    } else if (
+      typeof parseInt(display) === "number" &&
+      typeof text === "number"
+    ) {
+      // if the last character is a number, don't add another number
+      setDisplay(text);
+      setEquation(text);
     } else if (text === "AC") {
       setDisplay("0");
       setEquation("");
@@ -57,14 +63,20 @@ const Button = ({ text, setDisplay, display, equation, setEquation }) => {
       setEquation(text);
       setDisplay(text);
     } else if (text === "+/-") {
-      // Get the last number 
-      let lastNumDisplay = display.split(/ [+-x÷]+ /).slice(-1)
+      // Get the last number
+      let lastNumDisplay = display.split(/ [+-x÷]+ /).slice(-1);
 
       // Check if the last char is a number not operator
       // After that toggle the last number to negative or positive
       if (!"+-x÷".includes(lastNumDisplay)) {
-        display = display.replace(new RegExp(lastNumDisplay + '$'), lastNumDisplay * -1)
-        equation = equation.replace(new RegExp(lastNumDisplay + '$'), lastNumDisplay * -1)
+        display = display.replace(
+          new RegExp(lastNumDisplay + "$"),
+          lastNumDisplay * -1
+        );
+        equation = equation.replace(
+          new RegExp(lastNumDisplay + "$"),
+          lastNumDisplay * -1
+        );
       }
 
       setDisplay(display);
@@ -89,31 +101,27 @@ const Button = ({ text, setDisplay, display, equation, setEquation }) => {
       setEquation(equation + " ** "); // Added the power functionality
     } else if (text === "sin") {
       calculateTrigonometric("sin");
-    }else if (text === "cos") {
+    } else if (text === "cos") {
       calculateTrigonometric("cos");
-    }else if (text === "tan") {
+    } else if (text === "tan") {
       calculateTrigonometric("tan");
-    }
-    else if (text === "arcsin") {
+    } else if (text === "arcsin") {
       calculateTrigonometric("arcsin");
-    }
-    else if (text === "arccos") {
+    } else if (text === "arccos") {
       calculateTrigonometric("arccos");
-    }
-    else if (text === "arctan") {
+    } else if (text === "arctan") {
       calculateTrigonometric("arctan");
-    }
-    else if (text === "=") {
+    } else if (text === "=") {
       try {
         const result = Function(`'use strict'; return (${equation})`)();
-        if (!isFinite(result)){
-          throw new Error("Cannot divide by zero")
+        if (!isFinite(result)) {
+          throw new Error("Cannot divide by zero");
         }
         history.push(equation + " = " + result);
-        if(history.length > 10){
-            history.shift();
+        if (history.length > 10) {
+          history.shift();
         }
-        localStorage.setItem('history', JSON.stringify(history));
+        localStorage.setItem("history", JSON.stringify(history));
         setDisplay(result);
         setEquation(result);
       } catch (error) {
@@ -125,14 +133,13 @@ const Button = ({ text, setDisplay, display, equation, setEquation }) => {
       setDisplay(display === "0" ? text : display + text);
       setEquation(equation + text);
     }
-
-  }
+  };
 
   return (
-    <button className="button"
-      onClick={handleClick}
-    >{text}</button>
+    <button className="button" onClick={handleClick}>
+      {text}
+    </button>
   );
-}
+};
 
 export default Button;
